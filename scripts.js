2f4eb190e594702607340926e7290b356219da47
Nicolette <steenkampnicolette815@gmail.com>
2023-10-23 12:48:11 +0200

    html and some css and 1st JS function initialised

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// *Function to change the player names*/
+let player1 = "Player 1";
+let player2 = "Player 2";
+
+function editNames() {
+  player1 = prompt("Change Player1 name");
+  player2 = prompt("Change player2 name");
+
+  document.querySelector("p.Player1").innerHTML = player1;
+  document.querySelector("p.Player2").innerHTML = player2;
+}
